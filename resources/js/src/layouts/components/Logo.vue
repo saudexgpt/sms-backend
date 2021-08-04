eab0796eb6eaab6a9fe93b13118c1a72e784d991
sammy4best <saudexgpt@gmail.com>
2021-08-04 15:09:46 +0100

    update

--- resources/js/src/layouts/components/Logo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <img src="/images/logo/logo2.png" width="200" alt="">
-</template>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use App\Http\Controllers\ApplicationController;
 |
 */
 
-Route::get('/{any}', [ApplicationController::class, 'index'])->where('any', '^(?!api).*$');
+// Route::get('/{any}', [ApplicationController::class, 'index'])->where('any', '^(?!api).*$');
