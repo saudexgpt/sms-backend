cd69419ed4783583b72f1f85f7dd063e8691042f
sammy4best <saudexgpt@gmail.com>
2022-02-08 14:10:26 +0100

    fileSystem issue

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            // 'root' => storage_path('app/public'),
+            'root' => portalPulicPath(),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
