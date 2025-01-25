aac2071c6da3da37d56e482122c6127505c01e6f
Samuel Udoh <saudexgpt@gmail.com>
2025-01-25 21:54:06 +0100

    update middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,50 +2,16 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
      */
-    public function handle($request, Closure $next, ...$guards)
+    protected function redirectTo(Request $request): ?string
     {
-        if ($this->authenticate($request, $guards) === 'authentication_error') {
-            return response()->json(['error' => 'Unauthorized']);
-        }
-
-        return $next($request);
-    }
-
-    protected function authenticate($request, array $guards)
-    {
-        if (empty($guards)) {
-            $guards = [null];
-        }
-
-        foreach ($guards as $guard) {
-            if ($this->auth->guard($guard)->check()) {
-                return $this->auth->shouldUse($guard);
-            }
-        }
-
-        return 'authentication_error';
-    }
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (!$request->expectsJson()) {
-            return route('login');
-        }
+        return $request->expectsJson() ? null : route('login');
     }
 }
